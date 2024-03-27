9ba4459a134fdc35cd6e336be8c7ef4ca6de4abf
Dayan Ryspayev <izinor12@gmail.com>
2024-03-28 04:21:45 +0500

    Добавлен запрос

--- src/main/java/com/example/FoodExpress/repositories/RoleRepository.java
+++ src/main/java/com/example/FoodExpress/repositories/RoleRepository.java
@@ -1,8 +1,14 @@
 package com.example.FoodExpress.repositories;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
 import com.example.FoodExpress.models.Role;
 
 public interface RoleRepository extends CrudRepository<Role, Long>{
+    
+    @Query(value = "select * from Roles u where u.name = :name", nativeQuery = true)
+    public Role findRoleByName(@Param("name") String name);
     
 }
