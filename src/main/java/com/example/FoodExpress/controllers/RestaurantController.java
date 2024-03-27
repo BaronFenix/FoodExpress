ea067f49cd67e6bb815eda35cdc3ea264817d774
Dayan Ryspayev <izinor12@gmail.com>
2024-03-28 04:06:44 +0500

    Рефакторинг

--- src/main/java/com/example/FoodExpress/controllers/RestaurantController.java
+++ src/main/java/com/example/FoodExpress/controllers/RestaurantController.java
@@ -1,5 +1,10 @@
 package com.example.FoodExpress.controllers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,33 +12,83 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import com.example.FoodExpress.helpers.BasketManager;
+import com.example.FoodExpress.models.Category;
+import com.example.FoodExpress.models.Product;
 import com.example.FoodExpress.models.Restaurant;
+import com.example.FoodExpress.services.impl.ProductServiceImpl;
 import com.example.FoodExpress.services.impl.RestaurantServiceImpl;
 
 
 @Controller
-@SessionAttributes("authUser")
+@SessionAttributes({"authUser", "basket"})
 public class RestaurantController {
 
     @Autowired
     RestaurantServiceImpl restaurantService;
 
-    
-    @GetMapping("/restaurant/{name}")
-    public String restaurantView(@PathVariable(value = "name") String name, Model model) {
-        // if(!restaurantService.existsByName(name)) {
-        //     return "redirect:/not_found";
-        // }
+    @Autowired
+    ProductServiceImpl productService;
 
-        Optional<Restaurant> restaurant = restaurantService.getRestaurantByName(name);
-        if(restaurant.isPresent()) {
+    @Autowired
+    BasketManager basketManager;
+    
+    @GetMapping("/restaurant/{restaurantName}")
+    public String restaurantView(@PathVariable(value = "restaurantName") String restaurantName, Model model) {
+
+        Optional<Restaurant> restaurant = restaurantService.getRestaurantByName(restaurantName);
+        if(!restaurant.isPresent()) {
+            return "redirect:/";
+        }
+        else {
             model.addAttribute("restaurant", restaurant.get());
         }
 
+        List<Product> productsList = productService.getProductsByRestaurant(restaurantName);
+        List<Category> categoriesList = productService.getCategoriesByRestaurant(restaurantName);
+        Map<String, List<Product>> productsByCategory = getProductsMapByCategory(productsList);
+        
+        model.addAttribute("productsMapByCategory", productsByCategory);
+        model.addAttribute("categoriesList", categoriesList);
+        model.addAttribute("basket", model.getAttribute("basket"));
+
         return "restaurant";
     }
+
+
+    @PostMapping("/restaurant/{restaurantName}")
+    public String addToBasket(
+        @PathVariable(value = "restaurantName") String restaurantName,
+        @RequestParam Long selectedProductId,
+        @RequestParam int productQuantity,
+        Model model)
+        {
+            Product product = productService.getProductById(selectedProductId).get();
+            basketManager.addToBasket(product, productQuantity);
+            
+            model.addAttribute("basket", basketManager.getBasket());
+
+        return restaurantView(restaurantName, model);
+    }
+
     
+    private Map<String, List<Product>> getProductsMapByCategory(List<Product> products) {
+        Map<String, List<Product>> productsByCategory = new HashMap<>();
+
+        for(Product product : products) {
+            String categoryName = product.getCategory().getName();
+            if(!productsByCategory.containsKey(categoryName)) {
+                productsByCategory.put(categoryName, new ArrayList<>(Arrays.asList(product)));
+            }
+            else {
+                productsByCategory.get(categoryName).add(product);
+            }
+        }
+        return productsByCategory;
+    }
 
 }
--- src/main/java/com/example/FoodExpress/controllers/UserController.java
+++ src/main/java/com/example/FoodExpress/controllers/UserController.java
@@ -7,37 +7,52 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import com.example.FoodExpress.models.User;
 import com.example.FoodExpress.services.impl.UserServiceImpl;
+
+import jakarta.servlet.http.HttpSession;
+
 import org.springframework.web.bind.annotation.PostMapping;
 
 
 
 @Controller
-@SessionAttributes("authUser")
+@SessionAttributes({"authUser", "basket"})
 public class UserController {
     
     @Autowired
     UserServiceImpl userService;
 
-    @GetMapping("user/profile/{username}")
-    public String getMethodName(@PathVariable("username") String username, Model model) {
-        
-        model.addAttribute("authUser");
-        
+    @GetMapping("user/{username}/profile")
+    public String getMethodName(@PathVariable("username") String username, Model model, HttpSession session) {
+
+        model.addAttribute("authUser", session.getAttribute("authUser"));
         
         return "profile";
     }
 
 
-    @PostMapping("user/profile/{username}")
+    @PostMapping("user/{username}/profile")
     public String editUser(@PathVariable("username") String username, @ModelAttribute("authUser") User modifiedUser, Model model) {
         userService.editUser(modifiedUser);
         
         return "profile";
     }
-    
+
+    @GetMapping("/user/sign-out")
+    public String signOutView(Model model) {
+        return "sign-out";
+    }
+
+    @PostMapping("/user/sign-out")
+    public String signOut(Model model, HttpSession session, SessionStatus sessionStatus) {
+        session.invalidate();
+        sessionStatus.setComplete();
+
+        return "redirect:/";
+    }
     
 
 }
